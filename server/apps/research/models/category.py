04621013c74ffa697c330a1def86c27112ed7fc2
ndu <chijindujulian@gmail.com>
2024-10-10 05:20:26 +0100

    chore(fix): slugify category URL

--- server/apps/research/models/category.py
+++ server/apps/research/models/category.py
@@ -1,12 +1,19 @@
 from django.db import models
 from apps.common.models import BaseModel
+from django.utils.text import slugify
 
 class Category(BaseModel):
     """Model for categories."""
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
+        
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
