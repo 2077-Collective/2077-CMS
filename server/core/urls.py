8813648fb5a78ecaba0d651dde7ec0930573491a
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-11-29 14:32:53 +0100

    removed i18n config

--- server/core/urls.py
+++ server/core/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .token import csrf_token_view
-from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
@@ -19,9 +18,6 @@ urlpatterns = [
                   path('tinymce/', include('tinymce.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += i18n_patterns(
-    path('i18n/', include('django.conf.urls.i18n')),
-)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
