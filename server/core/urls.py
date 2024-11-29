51b175d82ef9214e64b4b7b2fd01f631330a2b75
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-11-29 13:23:58 +0100

    chore(fix): handled i18n internationalization for language chooser

--- server/core/urls.py
+++ server/core/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .token import csrf_token_view
+from django.conf.urls.i18n import i18n_patterns
+
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
@@ -16,6 +18,10 @@ urlpatterns = [
                   # TinyMCE URL
                   path('tinymce/', include('tinymce.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += i18n_patterns(
+    path('i18n/', include('django.conf.urls.i18n')),
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
