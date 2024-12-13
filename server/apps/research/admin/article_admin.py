80d6e670827db62be28267c31aa05d608ebf6eb0
ndu <chijindujulian@gmail.com>
2024-12-13 12:26:13 +0100

    chore(feat): add relate articles func

--- server/apps/research/admin/article_admin.py
+++ server/apps/research/admin/article_admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django import forms
+from django.db.models import Q
 from apps.research.models import Article, ArticleSlugHistory
 from tinymce.widgets import TinyMCE
 from .slug_history import current_slug_history
 
-
-    
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
@@ -13,32 +12,53 @@ class ArticleForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['acknowledgement'].widget = TinyMCE(attrs={'cols': 80, 'rows': 30, 'id': "acknowledgement_richtext_field", 'placeholder': f"Enter Acknowledgement here"})
-        self.fields['content'].widget = TinyMCE(attrs={'cols': 80, 'rows': 30, 'id': "content_richtext_field", 'placeholder': f"Enter Article Content here"})
+        
+        # Add filtering for related articles to exclude current article and drafts
+        if self.instance.pk:
+            self.fields['related_articles'].queryset = Article.objects.filter(
+                status='ready'
+            ).exclude(
+                Q(pk=self.instance.pk) | Q(status='draft')
+            ).order_by('-scheduled_publish_time')
+        
+        self.fields['acknowledgement'].widget = TinyMCE(attrs={'cols': 80, 'rows': 30, 'id': "acknowledgement_richtext_field", 'placeholder': "Enter Acknowledgement here"})
+        self.fields['content'].widget = TinyMCE(attrs={'cols': 80, 'rows': 30, 'id': "content_richtext_field", 'placeholder': "Enter Article Content here"})
 
 class ArticleAdmin(admin.ModelAdmin):
     """Admin interface for the Article model."""
     form = ArticleForm
+    
     def current_slug_history(self, obj):
         return current_slug_history(obj)
     current_slug_history.short_description = 'Slug Change History'
     
     fieldsets = [
-        ('Article Details', {'fields': ['title', 'slug', 'authors', 'acknowledgement', 'categories', 'thumb', 'content', 'summary', 'status', 'scheduled_publish_time']}),
-        ('Sponsorship Details', {'fields': ['is_sponsored', 'sponsor_color', 'sponsor_text_color']}),
+        ('Article Details', {
+            'fields': [
+                'title', 'slug', 'authors', 'acknowledgement', 'categories', 
+                'thumb', 'content', 'summary', 'status', 'scheduled_publish_time'
+            ]
+        }),
+        ('Related Content', {
+            'fields': ['related_articles'],
+            'description': 'Select up to 3 related articles that will appear at the end of this article'
+        }),
+        ('Sponsorship Details', {
+            'fields': ['is_sponsored', 'sponsor_color', 'sponsor_text_color']
+        }),
         ('URL Management', {
             'fields': ('current_slug_history',),
             'classes': ('collapse',),
             'description': 'History of URL changes for this article'
         }),
     ]
+    
     list_display = ('title', 'display_authors', 'status', 'views', 'display_categories', 'min_read', 'created_at', 'scheduled_publish_time')
     search_fields = ('title', 'authors__user__username', 'authors__twitter_username', 'content')
     list_per_page = 25
     list_filter = ('authors', 'status', 'categories', 'created_at', 'is_sponsored')
-    readonly_fields = ('views','current_slug_history',)
+    readonly_fields = ('views', 'current_slug_history',)
     list_editable = ('status',)
-    
 
     def display_authors(self, obj):
         """Return a comma-separated list of authors for the article."""
@@ -50,8 +70,8 @@ class ArticleAdmin(admin.ModelAdmin):
         return ", ".join(category.name for category in obj.categories.all())
     display_categories.short_description = 'Categories'
 
-    def save_model(self, request, obj, form, change):        
-        super().save_model(request, obj, form, change)  
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
 
     def has_change_permission(self, request, obj=None):
         """Check if the user has permission to change the article."""
@@ -72,6 +92,7 @@ class ArticleAdmin(admin.ModelAdmin):
         if obj is not None and not obj.authors.filter(user=request.user).exists():
             return False
         return True
+
 @admin.register(ArticleSlugHistory)
 class ArticleSlugHistoryAdmin(admin.ModelAdmin):
     """Admin interface for the ArticleSlugHistory model."""
@@ -93,5 +114,5 @@ class ArticleSlugHistoryAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False  # Prevent deletion
-    
+
 admin.site.register(Article, ArticleAdmin)
--- server/apps/research/serializers/article_serializer.py
+++ server/apps/research/serializers/article_serializer.py
@@ -3,13 +3,29 @@ from ..models import Article, Author, Category
 from .author_serializer import AuthorSerializer
 from .category_serializer import CategorySerializer
 
-class ArticleListSerializer(serializers.ModelSerializer):
-    categories = CategorySerializer(many=True)
+class RelatedArticleSerializer(serializers.ModelSerializer):
+    """Simplified serializer for related articles to prevent recursion"""
     authors = AuthorSerializer(many=True)
+    categories = CategorySerializer(many=True)
 
     class Meta:
         model = Article
-        include = ['categories' 'authors']
+        fields = [
+            'id', 'slug', 'title', 'authors', 'thumb', 'categories', 
+            'summary', 'min_read', 'created_at'
+        ]
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True)
+    authors = AuthorSerializer(many=True)
+    related_articles = serializers.SerializerMethodField()
+
+    def get_related_articles(self, obj):
+        related = obj.get_related_articles()
+        return RelatedArticleSerializer(related, many=True, context=self.context).data
+
+    class Meta:
+        model = Article
         exclude = [
             'content', 'scheduled_publish_time', 'acknowledgement', 
             'status', 'views', 'created_at', 'updated_at', 'table_of_contents'
@@ -18,33 +34,53 @@ class ArticleListSerializer(serializers.ModelSerializer):
 class ArticleSerializer(serializers.ModelSerializer):
     """Serializer for the Article model."""
     authors = AuthorSerializer(many=True, read_only=True)
-    categories = CategorySerializer(many=True)       
+    categories = CategorySerializer(many=True)
     views = serializers.ReadOnlyField()
     min_read = serializers.ReadOnlyField()
-    
+    related_articles = serializers.SerializerMethodField()
+
+    def get_related_articles(self, obj):
+        related = obj.get_related_articles()
+        return RelatedArticleSerializer(related, many=True, context=self.context).data
+
     class Meta:
         model = Article
         fields = [
             'id', 'slug', 'title', 'authors', 'thumb', 'categories', 'summary',
             'acknowledgement', 'content', 'min_read', 'status', 'views',
             'created_at', 'updated_at', 'scheduled_publish_time', 'table_of_contents',
-            'is_sponsored', 'sponsor_color', 'sponsor_text_color'
+            'is_sponsored', 'sponsor_color', 'sponsor_text_color', 'related_articles'
         ]
 
 class ArticleCreateUpdateSerializer(serializers.ModelSerializer):
     """Serializer for creating and updating articles."""
     authors = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), many=True, required=False)
     categories = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True, required=False)
+    related_articles = serializers.PrimaryKeyRelatedField(
+        queryset=Article.objects.filter(status='ready'),
+        many=True,
+        required=False
+    )
 
     class Meta:
         model = Article
-        fields = ['title', 'slug', 'categories', 'thumb', 'content', 'summary', 'acknowledgement', 'status', 'authors', 'scheduled_publish_time', 'is_sponsored', 'sponsor_color', 'sponsor_text_color']
+        fields = [
+            'title', 'slug', 'categories', 'thumb', 'content', 'summary', 
+            'acknowledgement', 'status', 'authors', 'scheduled_publish_time', 
+            'is_sponsored', 'sponsor_color', 'sponsor_text_color', 'related_articles'
+        ]
+    
+    def validate_related_articles(self, value):
+        if len(value) > 3:
+            raise serializers.ValidationError("You can select up to 3 related articles only.")
+        return value
     
     def create(self, validated_data: dict) -> Article:
         """Create a new article instance."""
         request = self.context.get('request')
         authors = validated_data.pop('authors', [])
         categories = validated_data.pop('categories', [])
+        related_articles = validated_data.pop('related_articles', [])
 
         try:
             if not authors and request and hasattr(request, 'user'):
@@ -59,16 +95,19 @@ class ArticleCreateUpdateSerializer(serializers.ModelSerializer):
                 article.authors.set(authors)
             if categories:
                 article.categories.set(categories)
+            if related_articles:
+                article.related_articles.set(related_articles)
 
             return article
         except Exception as e:            
             raise serializers.ValidationError(f"Error creating article: {str(e)}")
 
-
     def update(self, instance: Article, validated_data: dict) -> Article:
         """Update an existing article instance."""
         authors = validated_data.pop('authors', [])
         categories = validated_data.pop('categories', [])
+        related_articles = validated_data.pop('related_articles', [])
+
         try:
             instance = super().update(instance, validated_data)
 
@@ -76,6 +115,8 @@ class ArticleCreateUpdateSerializer(serializers.ModelSerializer):
                 instance.authors.set(authors)
             if categories:
                 instance.categories.set(categories)
+            if related_articles:
+                instance.related_articles.set(related_articles)
 
             return instance
         except Exception as e:          
