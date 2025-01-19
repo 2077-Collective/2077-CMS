be510d2a4cf2a2e74325269a70b48b71d917dad5
ndu <chijindujulian@gmail.com>
2025-01-19 20:03:30 +0100

    feat: add author endpoint + substack redirects

--- server/apps/research/redirects_urls.py
+++ server/apps/research/redirects_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    # Existing redirects
     path('api/articles/block-stm-vs-sealevel-1/', 
          RedirectView.as_view(url='/api/articles/block-stm_vs_sealevel/', permanent=True)),
     path('api/articles/eips-for-nerds-8-eip-7685/', 
@@ -10,4 +11,26 @@ urlpatterns = [
          RedirectView.as_view(url='/api/articles/EIP-7685-General-Purpose-Execution-Requests/', permanent=True)),
     path('api/articles/charting-ethereums-account-abstraction-roadmap-1/', 
          RedirectView.as_view(url='/api/articles/charting-ethereums-account-abstraction-roadmap-i-eip-3074-eip-7702/', permanent=True)),
+
+    # New redirects for Substack articles
+    path('p/eip-7503-zero-knowledge-wormholes/',
+         RedirectView.as_view(url='/eip-7503-zero-knowledge-wormholes-for-private-ethereum-transactions#introduction', permanent=True)),
+    path('p/eip-6110-supply-validator-deposits-onchain/',
+         RedirectView.as_view(url='/eip-6110-fixing-beacon-chain-tech-debt', permanent=True)),
+    path('p/eip-7251-increase-max-effective-balance/',
+         RedirectView.as_view(url='/eip-7251-raising-maximum-effective-balance-for-validators', permanent=True)),
+    path('p/a-guide-to-erc-7512-on-chain-representation/',
+         RedirectView.as_view(url='/erc-7512-bolstering-smart-contract-security-with-on-chain-audits', permanent=True)),
+    path('p/verkle-trees-for-the-rest-of-us-part-1/',
+         RedirectView.as_view(url='/verkle-trees', permanent=True)),
+    path('p/data-availability-in-ethereum-rollups/',
+         RedirectView.as_view(url='/data-availability-or-how-rollups-learned-to-stop-worrying-and-love-ethereum', permanent=True)),
+    path('p/eip-7002-execution-layer-exits/',
+         RedirectView.as_view(url='/eip-7002-unpacking-improvements-to-staking-ux-post-merge', permanent=True)),
+    path('p/eips-for-nerds-6-erc-5564-and-erc/',
+         RedirectView.as_view(url='/erc-5564-erc-6358-unlocking-privacy-on-ethereum-with-stealth-addresses', permanent=True)),
+    path('p/eip-7657-sync-committee-slashings/',
+         RedirectView.as_view(url='/eip-7657-securing-ethereums-sync-committee', permanent=True)),
+    path('p/understanding-blockchain-governance/',
+         RedirectView.as_view(url='/understanding-blockchain-governance', permanent=True)),
 ]
--- server/apps/research/urls.py
+++ server/apps/research/urls.py
@@ -3,20 +3,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
-from .views import ArticleViewSet
+from .views import ArticleViewSet, AuthorViewSet, tinymce_upload_image  # Import AuthorViewSet
 from .redirects_urls import urlpatterns as redirects_urlpatterns
-from .views import tinymce_upload_image
 from .rss import LatestArticlesFeed
 
+# Initialize the DefaultRouter
 router = DefaultRouter()
 router.register(r'articles', ArticleViewSet, basename='article')
+router.register(r'authors', AuthorViewSet, basename='author')  # Register AuthorViewSet
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/admin/', permanent=False)),  # Redirect root to admin
 
-     # custom redirect URL old slug to new slug
+    # Custom redirect URL old slug to new slug
     *redirects_urlpatterns,
     
+    # Include router URLs
     path('api/', include(router.urls)),
     
     # Custom URL for retrieving articles by slug or UUID
@@ -24,8 +26,10 @@ urlpatterns = [
    
     # Custom URL for retrieving articles by category
     re_path(r'^api/articles/category/(?P<category>[-\w]+)/$', ArticleViewSet.as_view({'get': 'retrieve_by_category'}), name='article-list-by-category'),
-    # upload tinyMCE images to cloudinary
+    
+    # Upload tinyMCE images to Cloudinary
     path('tinymce/upload/', tinymce_upload_image, name='tinymce_upload'),
-    #rss feed
+    
+    # RSS feed
     path('research/rss/', LatestArticlesFeed(), name='rss_feed'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
