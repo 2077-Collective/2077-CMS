8f481d8a92d4aebb52f3c2b3e2f42341c9f99f4d
ndu <chijindujulian@gmail.com>
2025-02-10 16:34:02 +0100

    fix: algolia

--- server/apps/research/urls.py
+++ server/apps/research/urls.py
@@ -1,4 +1,3 @@
-# urls.py
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,32 +7,20 @@ from .views import ArticleViewSet, AuthorViewSet, tinymce_upload_image
 from .redirects_urls import urlpatterns as redirects_urlpatterns
 from .rss import LatestArticlesFeed
 
-# Initialize the DefaultRouter
 router = DefaultRouter()
 router.register(r'articles', ArticleViewSet, basename='article')
 router.register(r'authors', AuthorViewSet, basename='author')
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/admin/', permanent=False)),
-
-    # Custom redirect URL old slug to new slug
     *redirects_urlpatterns,
-    
-    # Include router URLs
     path('api/', include(router.urls)),
-    
-    # Custom URL for retrieving articles by slug or UUID
-    re_path(r'^api/articles/(?P<identifier>[-\w0-9a-fA-F]+)/$', ArticleViewSet.as_view({'get': 'retrieve_by_identifier'}), name='article-detail-by-identifier'), 
-   
-    # Custom URL for retrieving articles by category
-    re_path(r'^api/articles/category/(?P<category_slug>[-\w]+)/$', ArticleViewSet.as_view({'get': 'retrieve_by_category'}), name='article-list-by-category'),
-    
-    # Custom URL for retrieving all categories (with optional filtering for primary categories)
-    re_path(r'^api/categories/$', ArticleViewSet.as_view({'get': 'categories'}), name='article-categories'),
-    
-    # Upload tinyMCE images to Cloudinary
     path('tinymce/upload/', tinymce_upload_image, name='tinymce_upload'),
-    
-    # RSS feed
+    re_path(r'^api/articles/category/(?P<category_slug>[-\w]+)/$', 
+            ArticleViewSet.as_view({'get': 'retrieve_by_category'}), 
+            name='article-list-by-category'),
+    re_path(r'^api/categories/$', 
+            ArticleViewSet.as_view({'get': 'categories'}), 
+            name='article-categories'),
     path('research/rss/', LatestArticlesFeed(), name='rss_feed'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
